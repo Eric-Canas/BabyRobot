b5cb9ca330b0d6d2e3b4f0b283cefe68a055039a
Eric Cañas <elcorreodeharu@gmail.com>
2020-10-17 11:34:14 +0200

    Package Installer for the Raspberry

---
+++ Raspinstall.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libatlas-base-dev -y
+pip3 install opencv-python
+pip3 install imutils
+pip3 install matplotlib
+pip3 scikit-learn
+pip3 scikit-image
+pip3 install numpy --upgrade
+pip3 install "picamera[array]"
