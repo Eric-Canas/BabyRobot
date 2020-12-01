f1e366f56f26feb8601baee1fdae8608cf5bf6bb
EricCanas <Eric.Canas@e-campus.uab.cat>
2020-12-01 13:42:27 +0100

    Prioritized reward

--- RobotController/ReinforcementLearningController/ReplayBuffer.py
+++ RobotController/ReinforcementLearningController/ReplayBuffer.py
@@ -2,14 +2,26 @@ from collections import deque
 from numpy import array, float32
 from random import sample
 from RobotController.RLConstants import DQN_REPLAY_BUFFER_CAPACITY, DQN_BATCH_SIZE
-
+import numpy as np
 
 class ReplayBuffer:
-    def __init__(self, capacity = DQN_REPLAY_BUFFER_CAPACITY):
+    def __init__(self, capacity = DQN_REPLAY_BUFFER_CAPACITY, usual_case_probability = 0.01):
         self.buffer = deque(maxlen=capacity)
+        self.capacity = capacity
+        self.usual_case_probability = usual_case_probability
+        if self.usual_case_probability is not None:
+            self.rewards = deque(maxlen=capacity)
 
     def push(self, state, action, reward, next_state):
-        self.buffer.append((state, action, reward, next_state))
+        is_useful = True
+        if len(self.buffer) > self.capacity*0.75  and self.usual_case_probability is not None:
+            mean, std = np.mean(self.rewards), np.std(self.rewards)
+            is_useful = (reward > mean) or (reward < (mean-std*2)) or (np.random.uniform() <= self.usual_case_probability)
+
+        if is_useful:
+            self.buffer.append((state, action, reward, next_state))
+            if self.usual_case_probability is not None:
+                self.rewards.append(reward)
 
     def sample(self, batch_size = DQN_BATCH_SIZE):
         state, action, reward, next_state = zip(*sample(self.buffer, batch_size))
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import sys
 MODES = ("CAPTURE_NEW_DATASET", "TRAIN_RECOGNIZER", "SHOW_DETECTIONS_DEMO", "TRAIN_MOVEMENT", "PLAY", "SERVER")
 execution_mode = "TRAIN_MOVEMENT"
 movement_mode = SYNC_MODE
-execute_on_server = True
+execute_on_server = False
 teleoperated_exploration = True
 
 if execution_mode.upper() == "CAPTURE_NEW_DATASET":
